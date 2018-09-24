fb3593c6b8342da1f92111134a475def1c92624e
Paul Lime <paullime@gmail.com>
2018-09-24 14:18:04 -0500

    config

--- config/config.js
+++ config/config.js
@@ -4,4 +4,6 @@ if (env === 'development') {
     process.env.MONGODB_URI = 'mongodb://localhost:27017/RecipeVault';
 } else if (env === 'test') {
     process.env.MONGODB_URI = 'mongodb://localhost:27017/RecipeVaultTest';
-}
+} else if (env === 'production') {
+    process.env.MONGODB_URI = 'mongodb://PaulL:pie4paul@ds257752.mlab.com:57752/recipe-vault';
+  }
