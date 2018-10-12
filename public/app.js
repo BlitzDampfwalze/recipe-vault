ce5046098085ee7afee0519a7b1baa76505c2ac6
Paul Lime <paullime@gmail.com>
2018-10-11 19:12:10 -0500

    added styling

--- public/app.js
+++ public/app.js
@@ -1,1 +1,31 @@
 checkLoggedin();
+
+$(() => {
+
+  const readmore = document.getElementsByClassName('accordion');
+
+
+  let i = null;
+
+  for (i = 0; i < readmore.length; i++) {
+    readmore[i].addEventListener('click', function () {
+      this.classList.toggle('active');
+
+      // document.getElementsByClassName('more').classList.toggle('background');
+
+      if (this.classList.contains('active')) {
+        $('.accordion').text('show less...')
+      } else {
+        $('.accordion').text('read more...')
+      }
+
+      let panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+
+})
